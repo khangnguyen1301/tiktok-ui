35518f6507c89e9a5bb162760a2cc993116d4a5f
duykhang1301 <khangnguyenpin@gmail.com>
2023-04-23 19:37:55 +0700

    Update Routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,7 @@ const routes = {
     upload: '/upload',
     search: '/search',
     live: '/live',
-    detailVideo: '/:nickname/:video/:videoID',
+    detailVideo: '/:nickname/video/:videoID',
     message: '/message',
 };
 
