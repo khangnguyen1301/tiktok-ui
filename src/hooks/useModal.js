d218bde3a7725b5c81ff43eb142f688163db2897
duykhang1301 <khangnguyenpin@gmail.com>
2023-04-25 19:49:03 +0700

    Update Show Login Modal

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,16 +1,21 @@
 import { useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
 import { VideoEnviroment } from '~/context/VideoContext/VideoContext';
+import { resetLogin, resetRegister } from '~/redux/authSlice';
 
 const useModal = (Modal) => {
     const [changeFile, setChangeFile] = useState(false);
     const [isShow, setIsShow] = useState(false);
 
     const videoContext = useContext(VideoEnviroment);
+    const dispatch = useDispatch();
 
     const handleShowModal = () => {
         document.body.style.overflow = 'hidden';
         setIsShow(true);
+        dispatch(resetRegister());
+        dispatch(resetLogin());
     };
 
     const handleHideModal = () => {
